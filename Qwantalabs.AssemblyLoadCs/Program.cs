855e144d45d620a85c00de613df53fcada259143
Carlos L. Perez <carlillos34@gmail.com>
2019-12-05 11:11:54 +0100

    Two versions of the same unloader. My only problem now is determining why the F# version weak reference is not working. But it will be part of another episode.

---
+++ Qwantalabs.AssemblyLoadCs/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Loader;
+
+namespace example
+{
+    class TestAssemblyLoadContext : AssemblyLoadContext
+    {
+        public TestAssemblyLoadContext() : base(true)
+        {
+        }
+
+        //protected override Assembly Load(AssemblyName name)
+        //{
+        //    return null;
+        //}
+    }
+
+    class Manager
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Execute(out WeakReference testAlcWeakRef)
+        {
+            var alc = new TestAssemblyLoadContext();
+            testAlcWeakRef = new WeakReference(alc);
+            alc.Resolving += (alc2, assemblyName) =>
+            {
+                var dllName = assemblyName.Name.Split(',').First();
+
+                var dllPath = dllName.Contains("FSharp.Core")
+                    ? @"C:\projects\FsFun\Qwantalabs.LoadAssemblies\bin\Release\netcoreapp3.1\FSharp.Core.dll"
+                    : @$"C:\projects\FsFun\Qwantalabs.DummyAssembly\bin\Debug\netstandard2.0\{dllName}.dll";
+                                
+                return alc2.LoadFromAssemblyPath(dllPath);
+            };
+
+//            Assembly a = alc.LoadFromAssemblyPath("C:\\projects\\FsFun\\Qwantalabs.DummyAssembly\\bin\\Debug\\netstandard2.0\\Qwantalabs.DummyAssembly.dll");
+            var assembly = alc.LoadFromAssemblyPath("C:\\projects\\FsFun\\Qwantalabs.DummyAssembly\\bin\\Debug\\netstandard2.0\\Qwantalabs.DummyAssembly.dll");
+            var myType = assembly.GetType("Qwantalabs.DummyAssembly.Say", true);
+            var method = myType.GetMethod("hello", BindingFlags.Public | BindingFlags.Static);
+            var result = method.Invoke(null, new Object[] {"Carlos"});
+
+            alc.Unload();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Unload(WeakReference testAlcWeakRef)
+        {
+            for (int i = 0; testAlcWeakRef.IsAlive && (i < 10); i++)
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+
+            Console.WriteLine($"is alive: {testAlcWeakRef.IsAlive}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manager = new Manager();
+            manager.Execute(out var testAlcWeakRef);
+            manager.Unload(testAlcWeakRef);
+        }
+    }
+}
--- Qwantalabs.LoadAssemblies/Program.fs
+++ Qwantalabs.LoadAssemblies/Program.fs
@@ -1,20 +1,55 @@
 ﻿// Learn more about F# at http://fsharp.org
 
+//https://docs.microsoft.com/en-us/dotnet/core/dependency-loading/understanding-assemblyloadcontext
+
 open System
 open System.Runtime.Loader
+
+#if DEBUG
+let assemblyPath = "C:\\projects\\FsFun\\Qwantalabs.DummyAssembly\\bin\\Debug\\netstandard2.0\\Qwantalabs.DummyAssembly.dll";
+#else
+let assemblyPath = "C:\\projects\\FsFun\\Qwantalabs.DummyAssembly\\bin\\Release\\netstandard2.0\\Qwantalabs.DummyAssembly.dll";
+#endif
 
 type MyAssemblyLoader() = 
     inherit AssemblyLoadContext(true)
 
+let DoIt (loaderRef : WeakReference<AssemblyLoadContext> ) =
+    try
+        let success, loader = loaderRef.TryGetTarget()
+        match success, loader with
+        | false, _ -> Some("This is Broken")
+        | true , loader ->  let assembly = loader.LoadFromAssemblyPath (assemblyPath)
+                            let myType = assembly.GetType("Qwantalabs.DummyAssembly.Say", true) 
+                            let method = myType.GetMethod("hello", Reflection.BindingFlags.Public ||| Reflection.BindingFlags.Static)
+                            method.Invoke(null, [|"Carlos"|]) |> ignore
+                            None
+    with
+        | _ -> Some("Something broke")
 
+
+let rec CollectAndCheckRec (weakRef : WeakReference<AssemblyLoadContext>) counter =
+    match weakRef.TryGetTarget() with
+    | false, _      -> printfn "Counter: %d. DONE" counter
+    | true,  _      -> match counter with 
+                        | 0        ->   printfn "%A" "Number of Attempts depleted" 
+                        | x as int ->   GC.Collect() 
+                                        GC.WaitForPendingFinalizers() 
+                                        printfn "Counter: %d" counter
+                                        CollectAndCheckRec weakRef (counter - 1)
+
+let CollectAndCheck weakRef =
+    CollectAndCheckRec weakRef 10
+                                
 [<EntryPoint>]
 let main argv =
-    let myloader = MyAssemblyLoader()
-    printfn "Loading an assembly"
+    let myLoaderUploaded = upcast MyAssemblyLoader(): AssemblyLoadContext
+    let testAlcWeakRef = new WeakReference<AssemblyLoadContext>(myLoaderUploaded);
+    let result = DoIt testAlcWeakRef 
+    myLoaderUploaded.Unload()
+    myLoaderUploaded = null |> ignore;
+    let xxx = CollectAndCheck testAlcWeakRef
+    0
+
     
-    let assembly = myloader.LoadFromAssemblyPath ("C:\\projects\\FsFun\\Qwantalabs.DummyAssembly\\Qwantalabs.DummyAssembly.dll");
-    let myType = assembly.GetType("Qwantalabs.DummyAssembly.Say", true) 
-    let method = myType.GetMethod("hello", Reflection.BindingFlags.Static)
-    let result = method.Invoke(null,null)    
-    myloader.Unload()
-    0 // return an integer exit code
+    
